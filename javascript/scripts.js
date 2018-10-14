1a1f9305e1a2c9d0426739d43c64825531d4e13d
akasake <pham_see@hotmail.com>
2018-10-14 17:06:25 +0200

    decided to work in laravel afterall haHAA

--- javascript/scripts.js
+++
@@ -1,3 +0,0 @@
-function pythagorean(sideA, sideB){
-    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-  }
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/app.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700i" rel="stylesheet">
+        <title>Pham Akasake</title>
+    </head>
+    <body>
+        <header>
+            <img src="/images/logo color.svg" alt="logo" class="logo">
+            <h1>Webdevelopment and Design</h1>
+        </header>
+        <div class="container">
+            <nav>
+                <!--<div class="box">
+                    <a href="#">About me</a>
+                    <a href="#">My work</a>
+                    <a href="#">Contact</a>
+                </div>-->
+            </nav>
+            <div class="about">
+                <h1 class="fntRed fntRight">About me</h1>
+                <p class="fntRight">
+                        My name is Akasake, or Pham. I study Interactive multimedia design at SomeSchool. 
+                        You can usually find me coding, designing, eating good food, gaming, traveling or reading a good book. Aside from what I usually do, I also like to paint, write, play piano or learn any new skill I am interested i
+                </p>
+            </div>
+            <div class="skills">
+                <h1 class="fntLeft">My skills</h1>
+            </div>
+            <div class="work">
+                <h1 class="fntRed fntLeft">My work</h1>
+            </div>
+            <div class="contact">
+                <h1>Contact</h1>
+                <p>Want to work with me? <br>
+                        Or simply just want to say hi?<br>
+                        Email me, or find me on my social media accounts linked below. <br>
+                        Talk to you soon!</p>
+            </div>
+        </div>    
+    </body> 
+</html>
