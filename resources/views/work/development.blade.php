6c040748c86863c7312d7c7acdd9ba04e113f575
akasake <pham_see@hotmail.com>
2018-10-16 23:27:25 +0200

    css design and dev work list pages done

--- resources/views/work/development.blade.php
+++ resources/views/work/development.blade.php
@@ -15,41 +15,42 @@
         </div>  
 @endsection
 @section('main')
-    {{$a = 1;}}
-    @foreach( $works as $w)
-        @if($a === 1)
-            <div class = "purple left"> 
-                <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
-                <h2>{{$w->title}}</h2>
-                <p>{{$w->description}}</p>
-                <a href="/work/{{$w->id}}">read more</a>
-                {{$a = 2}}
-            </div>
-        @elseif($a === 2)
-            <div class = "blue right"> 
-                <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
-                <h2>{{$w->title}}</h2>
-                <p>{{$w->description}}</p>
-                <a href="/work/{{$w->id}}">read more</a>
-                {{$a = 3}}
-            </div>
-        @elseif($a ===3)
-            <div class = "beige left"> 
-                <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
-                <h2>{{$w->title}}</h2>
-                <p>{{$w->description}}</p>
-                <a href="/work/{{$w->id}}">read more</a>
-                {{$a = 4}}
-            </div>
-        @elseif($a===4)
-            <div class = "green right"> 
-                <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
-                <h2>{{$w->title}}</h2>
-                <p>{{$w->description}}</p>
-                <a href="/work/{{$w->id}}">read more</a>
-                {{$a = 1}}
-            </div>
-        @endif
-
-    @endforeach
+    <div class="work_display">
+        <p class="hidden">{{$key = 1}}</p>
+        @foreach( $works as $w)
+            @if($key === 1 )
+                <div class = "right"> 
+                    <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
+                    <h2 class="fntPurple">{{$w->title}}</h2>
+                    <p>{{$w->description}}</p>
+                    
+                </div>
+                <p class="hidden">{{$key = 2}}</p>
+            @elseif($key ===2)
+                <div class = "left"> 
+                    <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
+                    <h2 class="fntBlue">{{$w->title}}</h2>
+                    <p>{{$w->description}}</p>
+                    
+                </div>
+                <p class="hidden">{{$key = 3}}</p>
+            @elseif($key === 3)
+                <div class = "right"> 
+                    <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
+                    <h2 class="fntBeige">{{$w->title}}</h2>
+                    <p>{{$w->description}}</p>
+                     
+                </div>
+                <p class="hidden">{{$key = 4}}</p>
+            @elseif($key === 4)
+                <div class = "left"> 
+                    <img src="{{$w->cover_url}}" alt="{{$w->title}} cover image">
+                    <h2 class="fntGreen">{{$w->title}}</h2>
+                    <p>{{$w->description}}</p>
+                    
+                </div>
+                <p class="hidden">{{$key = 1}}</p>
+            @endif
+        @endforeach
+    </div>
 @endsection
